11952b7384021c5961e164fea9558db8ed9295b4
alenavee <alenavarg@mail.ru>
2021-10-08 18:13:50 +0300

    add first task

---
+++ Assignment_0/PaymentCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PaymentCalculator
+{
+    class Program
+    {
+
+        static double CalculatePrice(int price, int quantity, int discount, int tax)
+        {
+            double totalPrice = price * quantity * (1 - discount * 0.01) * (1 + tax * 0.01);
+            return totalPrice;
+        }
+
+
+        static void Main(string[] args)
+        {
+            double totalPrice = CalculatePrice(7, 5, 4, 21);
+            Console.WriteLine("The total price is {0}", totalPrice);
+        }
+    }
+
+}
